26c1275398349cec94ba3bfcd3f94b0e4ce629a1
Henrique alves de oliveira <henriquealvesoliveira@hotmail.com>
2016-06-17 01:57:47 -0300

    Divs das imagens do anuncio e dos quartos

--- Repub/js/createAnnouncement.js
+++ Repub/js/createAnnouncement.js
@@ -57,35 +57,44 @@
 
     var imagesDiv = document.createElement('div');
 
+    var imagesRoomDiv0 = document.createElement('div');
+    imagesRoomDiv0.id = 'div-quarto-' + count + '-imagem[0]';
+
+    var imageLabel0 = document.createElement('label');
+    imageLabel0.className = 'images-input';
+    imageLabel0.innerHTML = 'Imagem 1';
+
+    var imageInput0 = document.createElement('input');
+    imageInput0.className = 'images-input-field default-button';
+    imageInput0.type = 'file';
+    imageInput0.name = 'quarto-' + count + '-imagem[0]';
+    imageInput0.id = 'quarto-' + count + '-imagem[0]';
+
+    var imagesRoomDiv1 = document.createElement('div');
+    imagesRoomDiv1.id = 'div-quarto-' + count + '-imagem[1]';
+
     var imageLabel1 = document.createElement('label');
     imageLabel1.className = 'images-input';
-    imageLabel1.innerHTML = 'Imagem 1';
+    imageLabel1.innerHTML = 'Imagem 2';
 
     var imageInput1 = document.createElement('input');
     imageInput1.className = 'images-input-field default-button';
     imageInput1.type = 'file';
-    imageInput1.name = 'quarto-' + count + '-imagem[0]';
-    imageInput1.id = 'quarto-' + count + '-imagem[0]';
+    imageInput1.name = 'quarto-' + count + '-imagem[1]';
+    imageInput1.id = 'quarto-' + count + '-imagem[1]';
 
+    var imagesRoomDiv2 = document.createElement('div');
+    imagesRoomDiv2.id = 'div-quarto-' + count + '-imagem[2]';
+    
     var imageLabel2 = document.createElement('label');
     imageLabel2.className = 'images-input';
-    imageLabel2.innerHTML = 'Imagem 2';
+    imageLabel2.innerHTML = 'Imagem 3';
 
     var imageInput2 = document.createElement('input');
     imageInput2.className = 'images-input-field default-button';
     imageInput2.type = 'file';
-    imageInput2.name = 'quarto-' + count + '-imagem[1]';
-    imageInput2.id = 'quarto-' + count + '-imagem[1]';
-
-    var imageLabel3 = document.createElement('label');
-    imageLabel3.className = 'images-input';
-    imageLabel3.innerHTML = 'Imagem 3';
-
-    var imageInput3 = document.createElement('input');
-    imageInput3.className = 'images-input-field default-button';
-    imageInput3.type = 'file';
-    imageInput3.name = 'quarto-' + count + '-imagem[2]';
-    imageInput3.id = 'quarto-' + count + '-imagem[2]';
+    imageInput2.name = 'quarto-' + count + '-imagem[2]';
+    imageInput2.id = 'quarto-' + count + '-imagem[2]';
 
     var borderDiv = document.createElement('div');
     borderDiv.className = 'room-border';
@@ -101,12 +110,15 @@
     rentDiv.appendChild(rentFalse);
     rentDiv.appendChild(spanFalse);
     rentDiv.appendChild(imagesDiv);
-    imagesDiv.appendChild(imageLabel1);
-    imagesDiv.appendChild(imageInput1);
-    imagesDiv.appendChild(imageLabel2);
-    imagesDiv.appendChild(imageInput2);
-    imagesDiv.appendChild(imageLabel3);
-    imagesDiv.appendChild(imageInput3);
+    imagesRoomDiv0.appendChild(imageLabel0);
+    imagesRoomDiv0.appendChild(imageInput0);
+    imagesRoomDiv1.appendChild(imageLabel1);
+    imagesRoomDiv1.appendChild(imageInput1);
+    imagesRoomDiv2.appendChild(imageLabel2);
+    imagesRoomDiv2.appendChild(imageInput2);
+    imagesDiv.appendChild(imagesRoomDiv0);
+    imagesDiv.appendChild(imagesRoomDiv1);
+    imagesDiv.appendChild(imagesRoomDiv2);
     rentDiv.appendChild(borderDiv);
     rooms.appendChild(roomDiv);
 
