642aea9f66e13e381ca72854b6cb7e7ce66918c0
Gabriel Marquesi <gabriel.mmarquesi@gmail.com>
2016-06-18 10:05:41 -0300

    anuncio.css e anuncio.html

--- Repub/js/editAnnouncement.js
+++ Repub/js/editAnnouncement.js
@@ -37,15 +37,39 @@
     internet.value = anuncio.internet;
 
     for (i = 0; i < anuncio.imagens.length; i++) {
-        var img = document.createElement('img');
-        img.src = anuncio.imagens[i];
-        img.style.width = 48;
-        img.style.height = 48;
+        var imgDiv = document.createElement('div');
+        imgDiv.id = 'anuncio-imagem-thumbnail-div[' + i + ']';
+        imgDiv.name = 'anuncio-imagem-thumbnail-div[' + i + ']';
+        imgDiv.className = 'images-input-field';
+
+        var removeImg = document.createElement('div');
+        removeImg.id = 'anuncio-imagem-thumbnail-delete[' + i + ']';
+        removeImg.name = 'anuncio-imagem-thumbnail-delete[' + i + ']';
+        removeImg.className = 'editar-anuncio-imagem-thumbnail-removeicon';
+        removeImg.onclick = function () {
+            var id = this.id.replace('-thumbnail-delete', '');
+            var unhide = document.getElementById(id);
+            unhide.hidden = false;
+            unhide.style.display = '';
+            var container = this.parentElement.parentElement;
+            container.remove();
+        };
+
+        var img = document.createElement('div');
+        img.className = "image-preview";
+        img.id = 'anuncio-imagem-preview[' + i + ']';
+        img.name = 'anuncio-imagem-preview[' + i + ']';
+        img.className = 'editar-anuncio-imagem-thumbnail';
+        img.style.backgroundImage = 'url(' + anuncio.imagens[i] + ')';
 
         var anuncioImagem = document.getElementById('anuncio-imagem[' + i + ']');
         anuncioImagem.hidden = true;
+        anuncioImagem.style.display = 'none';
+
         var anuncioImagemDiv = document.getElementById('div-anuncio-imagem[' + i + ']');
-        anuncioImagemDiv.appendChild(img);
+        imgDiv.appendChild(img);
+        img.appendChild(removeImg);
+        anuncioImagemDiv.appendChild(imgDiv);
     }
 
     for (i = 0; i < anuncio.quartos.length; i++) {
@@ -65,8 +89,39 @@
         quartoAlugadoFalse.value = !anuncio.quartos[i].alugado;
 
         for (j = 0; j < anuncio.quartos[i].imagens.length; j++) {
+            var imgDiv = document.createElement('div');
+            imgDiv.id = 'quarto-' + i + '-imagem-thumbnail-div[' + j + ']';
+            imgDiv.name = 'quarto-' + i + '-imagem-thumbnail-div[' + j + ']';
+            imgDiv.className = 'images-input-field';
+
+            var removeImg = document.createElement('div');
+            removeImg.id = 'quarto-' + i + '-imagem-thumbnail-delete[' + j + ']';
+            removeImg.name = 'quarto-' + i + '-imagem-thumbnail-delete[' + j + ']';
+            removeImg.className = 'editar-anuncio-imagem-thumbnail-removeicon';
+            removeImg.onclick = function () {
+                var id = this.id.replace('-thumbnail-delete', '');
+                var unhide = document.getElementById(id);
+                unhide.hidden = false;
+                unhide.style.display = '';
+                var container = this.parentElement.parentElement;
+                container.remove();
+            };
+
+            var img = document.createElement('div');
+            img.className = "image-preview";
+            img.id = 'quarto' + i + '-imagem-preview[' + j + ']';
+            img.name = 'quarto-' + i + '-imagem-preview[' + j + ']';
+            img.className = 'editar-anuncio-imagem-thumbnail';            
+            img.style.backgroundImage = 'url(' + anuncio.quartos[i].imagens[j] + ')';
+
             var quartoImagem = document.getElementById('quarto-' + i + '-imagem[' + j + ']');
-            quartoImagem.value = anuncio.quartos[i].imagens[j];
+            quartoImagem.hidden = true;
+            quartoImagem.style.display = 'none';
+
+            var quartoImagemDiv = document.getElementById('div-quarto-' + i + '-imagem[' + j + ']');
+            imgDiv.appendChild(img);
+            img.appendChild(removeImg);
+            quartoImagemDiv.appendChild(imgDiv);
         }
     }
 }
